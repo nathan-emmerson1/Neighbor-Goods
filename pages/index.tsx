b426c13d9782b861dcd114da37ffe6367729256c
Nathan Emmerson <tiopira98@gmail.com>
2024-06-19 20:54:46 +1200

    added Profile-page component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import ProfilePage from './profilepage'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+    <ProfilePage />
+    </div>
+    
   )
 }
 
--- pages/profilepage.tsx
+++ pages/profilepage.tsx
@@ -1,16 +1,42 @@
 'use client'
 
 
+import { useQuery } from '@tanstack/react-query'
 import react from 'react'
 
+// This is userData
+const user = {
+id: 1,
+name: 'mrs jaming',
+age: 23,
+email: 'johnny@gmail.com',
+phone: '0238849459',
+location: 'jump around',
+img: 'www.getimage.com',
+
+}
 
 function ProfilePage() {
-  
-  
-  return (
-    <>
+  // Query need to be hook up to api
+  const {data, isPending, isError} = useQuery({
+    queryKey: ['user'],
+    queryFn: () => fetchUserById(id),
     
-    </>
+  })
+  if(isPending) return <p>Loading...</p>
+  if(isError) return <p>There was a error</p>
+  console.log(user)
+  // useing all user data
+  return (
+ <div>
+ <p>{user.img}</p>
+ <p>{user.name}</p>
+ <p>{user.email}</p>
+ <p>{user.phone}</p>
+ <p>{user.location}</p>
+ </div>
+
+ 
   )
 
 }
