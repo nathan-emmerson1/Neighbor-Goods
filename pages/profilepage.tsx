1356db50dae0dfbda698c90872968ec80555ed82
Nathan Emmerson <tiopira98@gmail.com>
2024-06-20 21:29:56 +1200

    added to profile-page

--- pages/profilepage.tsx
+++ pages/profilepage.tsx
@@ -9,25 +9,10 @@ function ProfilePage() {
 
   return (
     <div>
-    <div></div>
-    <div></div>
-    <div></div>
-    
+      <div>profilepage</div>
+      <div></div>
+      <div></div>
     </div>
-    
-
-v
- 
-       
   )
 }
 export default ProfilePage
-{/* <div>
-    <div classNameName="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-    <img src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' />
-    </div>
-      <div>{session.data?.user?.name}</div>
-      <div>{session.data?.user?.email}</div>
-      
-      
-    </div> */}
