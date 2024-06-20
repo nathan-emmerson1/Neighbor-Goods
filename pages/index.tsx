70451e27f229d847c42999aaea7b67dbef93d1d5
ShaneSaw1705 <shanecurtis1705@gmail.com>
2024-06-20 16:38:37 +1200

    Added button for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useSession } from 'next-auth/react'
 export default function Home() {
   const session = useSession()
   return (
-    <div>{session.data?.user?.name}</div>
+    <div>{session.data?.user?.name}<button className='btn btn-primary'>test</button></div>
   )
 }
 
