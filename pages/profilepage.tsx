f775cc31a2a91d8c5fa634399573e66b2e9f09c9
Nathan Emmerson <tiopira98@gmail.com>
2024-06-21 09:37:45 +1200

    added to profile-pege

--- pages/profilepage.tsx
+++ pages/profilepage.tsx
@@ -2,16 +2,27 @@
 import react from 'react'
 
 import { useSession } from 'next-auth/react'
+import { userAgent } from 'next/server'
+import { UserData } from '@/models/user'
+
+const user: UserData = {
+  name: 'johnny',
+  age: 26,
+  email: 'johnnyy0889@gmail.com',
+  phone: '0223634747',
+  location: 'where is there'
+}
 
 function ProfilePage() {
   const session = useSession()
-  console.log(session.data)
+  console.log(session.data?.user?.name)
 
   return (
     <div>
-      <div>profilepage</div>
-      <div></div>
-      <div></div>
+      <div>{user.name}</div>
+      <div>{user.age}</div>
+      <div>{user.email}</div>
+      <div>{user.phone}</div>
     </div>
   )
 }
