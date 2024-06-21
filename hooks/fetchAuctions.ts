3b64145b17a40ddd682ef592ee3bb82fa13fb990
ShaneSaw1705 <shanecurtis1705@gmail.com>
2024-06-21 15:53:27 +1200

    Added a couple of front end hooks for recieving auctions and jobs

---
+++ hooks/fetchAuctions.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import request from "superagent";
+
+export function useAuctions(pageNum: number) {
+  return useQuery({
+    queryKey: ["auctions", pageNum],
+    queryFn: async () => {
+      const data = await request.get("api/jobs/getjobs");
+      return data;
+    },
+  });
+}
