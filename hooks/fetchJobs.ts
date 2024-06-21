9f8c616fe2f8d64c0362946627eb006d8a9ab66e
ShaneSaw1705 <shanecurtis1705@gmail.com>
2024-06-21 15:38:50 +1200

    Changed schema again

---
+++ hooks/fetchJobs.ts
@@ -0,0 +1,10 @@
+// import { useQuery } from "@tanstack/react-query";
+// import request from "superagent";
+// export function useJobs(pageNum: number) {
+//   return useQuery({
+//     queryKey: ['jobs', pageNum],
+//     queryFn: () => {
+//       const data = await
+//     }
+//   })
+// }
