758bc19701291dff63469b40a04ea683209ca1bf
Nathan Emmerson <tiopira98@gmail.com>
2024-06-22 20:58:06 +1200

    added more css

--- pages/profilepage.tsx
+++ pages/profilepage.tsx
@@ -18,23 +18,58 @@ function ProfilePage() {
   console.log(session.data?.user)
 
   return (
+    <div>
+    <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a href='http://localhost:3000/home' className="btn btn-ghost text-xl">Neighbours Goods</a>
+  </div>
+  <div className="flex-none gap-2">
+    <div className="form-control">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+    </div>
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img  src={session.data?.user?.image}
+                  alt={session.data?.user?.image} />
+        </div>
+      </div>
+      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a href='http://localhost:3000/home'>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+    
+
+
+
     <div
       className="hero min-h-screen"
       style={{ backgroundImage: 'url(/images/background-image.png' }}
     >
-      <div className=" border-solid border-4 border-secondary mt-40 relative max-w-md mx-auto md:max-w-1xl  min-w-0 break-words bg-white w-full  shadow-lg rounded-xl ">
+    
+      <div className=" border-solid border-4 border-secondary mt-34 relative max-w-md mx-auto md:max-w-1xl  min-w-0 break-words bg-white w-full  shadow-lg rounded-xl ">
         <div className="px-6 ">
           <div className="flex flex-wrap justify-center">
             <div className="w-full flex justify-center">
               <div className="relative">
                 <img
-                  src={session.data?.user?.image} alt={session.data?.user?.image}
+                  src={session.data?.user?.image}
+                  alt={session.data?.user?.image}
                   className=" border-solid border-4 border-secondary shadow-xl rounded-full align-middle absolute -m-16 -ml-17 lg:-ml-14 max-w-[150px] "
                 />
               </div>
             </div>
-            <div className="border-solid border-4 border-neutral w-full  text-center mt-20 mb-6">
-              <div className="flex justify-center lg:pt-4 pt-3 pb-0">
+            <div className="border-solid border-4 border-secondary w-full  text-center mt-20 mb-10">
+              <div className="flex justify-center lg:pt-4 pt-3 pb-3">
                 <div className="p-3 text-center p ">
                   <span className=" text-xl font-bold block uppercase tracking-wide text-slate-700">
                     3,360
@@ -57,25 +92,23 @@ function ProfilePage() {
               </div>
             </div>
           </div>
-          <div className="text-center mt-2">
-            <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-1">
+           <div className="mt-2 py-2 border-t border-accent text-center"></div>
+          <div className="  text-center mt-2">
+            <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-4">
               {session.data?.user?.name}
             </h3>
             <div className="text-xs mt-0 mb-2 text-slate-400 font-bold uppercase">
-              <i className="fas fa-map-marker-alt mr-2  opacity-75 text-slate-primary "></i>
+              <i className=" fa-map-marker-alt mr-2  opacity-75 text-slate-primary "></i>
               Email:{session.data?.user?.email}
             </div>
           </div>
-          <div className="mt-6 py-6 border-t border-slate-200 text-center">
+          <div className="mt-6 py-6 border-t border-accent text-center">
             <div className="flex flex-wrap justify-center">
               <div className="w-full px-4">
                 <p className="font-light leading-relaxed text-slate-600 mb-4">
                   i want to learn how to play guiter
                 </p>
-                <a
-                  href="javascript:;"
-                  className="btn btn-primary  font-normal"
-                >
+                <a href="javascript:;" className="btn btn-primary  font-normal">
                   Edit Profile
                 </a>
                 <a
@@ -89,6 +122,7 @@ function ProfilePage() {
           </div>
         </div>
       </div>
+    </div>
     </div>
   )
 }
