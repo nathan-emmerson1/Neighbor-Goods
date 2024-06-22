95e13beb4387bccada212404e87d8ae8a2f92328
Nathan Emmerson <tiopira98@gmail.com>
2024-06-22 21:31:07 +1200

    conflicts changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-<<<<<<< HEAD
-import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
-=======
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
->>>>>>> main
 import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient()
 
-<<<<<<< HEAD
-export default function App({Component, pageProps: { session, ...pageProps }}: AppProps) {
-  
-  return (
-    <SessionProvider session={session}>
-      <QueryClientProvider client={queryClient} >
-      <Component {...pageProps}/>
-      </QueryClientProvider>
-    </SessionProvider>
-=======
 export default function App({ Component, pageProps: { session, ...pageProps }}: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
@@ -26,6 +12,5 @@ export default function App({ Component, pageProps: { session, ...pageProps }}: 
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
->>>>>>> main
   )
 }
