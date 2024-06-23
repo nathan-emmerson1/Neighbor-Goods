3379c8bef6f654064cdfb46c358fe0dff06a9b67
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-06-23 13:11:11 +1200

    added google signup to the signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
+import { signIn } from "next-auth/react";
 
 export default function SignUp() {
   const router = useRouter();
   const [email, setEmail] = useState("");
-  const [name, setName] = useState("")
 
   const signInWithGoogle = () => {
     // Google sign up logic here
+    signIn('google')
   };
 
   const signUpWithEmail = () => {
@@ -30,18 +31,6 @@ export default function SignUp() {
               <div className="card-body">
                 <h2 className="card-title title-center">Create an Account</h2>
                 <form>
-                  <label className="input input-bordered flex items-center gap-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 16 16"
-                      fill="currentColor"
-                      className="w-4 h-4 opacity-70"
-                    >
-                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                    </svg>
-                    <input type="text" className="grow" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
-                  </label>
-
                   <label className="input input-bordered flex items-center gap-2">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
