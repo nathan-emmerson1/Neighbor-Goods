f9faa3fc021afd9d7ff1b0943a27eb1d1bbd87e1
Nathan Emmerson <tiopira98@gmail.com>
2024-06-23 14:20:40 +1200

    added changes

--- pages/profilepage.tsx
+++ pages/profilepage.tsx
@@ -18,35 +18,6 @@ function ProfilePage() {
   console.log(session.data?.user)
 
   return (
-    <div>
-    <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a href='http://localhost:3000/home' className="btn btn-ghost text-xl">Neighbours Goods</a>
-  </div>
-  <div className="flex-none gap-2">
-    <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    </div>
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img  src={session.data?.user?.image}
-                  alt={session.data?.user?.image} />
-        </div>
-      </div>
-      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a href='http://localhost:3000/home'>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
     
 
 
@@ -123,7 +94,7 @@ function ProfilePage() {
         </div>
       </div>
     </div>
-    </div>
+    
   )
 }
 export default ProfilePage
