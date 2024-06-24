9964871b54dedce276cb5b60503a08201f0595c9
Jacob Wingate <jacob.wingate33@gmail.com>
2024-06-24 15:48:43 +1200

    add individual job page, styling and a few jobs to db

--- components/AuctionList.tsx
+++ components/AuctionList.tsx
@@ -18,38 +18,53 @@ export default function AuctionList(props: Props) {
   }
 
   return (
-    <div className="grid grid-cols-3 gap-4 px-10 mb-10">
-      {auctions?.map((auction) => (
-        <div key={auction.name} className="card w-96 bg-base-100 shadow-xl">
-          <figure className="px-10 pt-10">
-            <img src="https://5.imimg.com/data5/WS/LU/MY-39504911/mens-woolen-sweater.jpg" />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">{auction.name}</h2>
-            <p>{auction.description}</p>
-            <h3 className="card-title">Compensation: {auction.compensation}</h3>
-            <ul>
-              <li>
-                <strong>Restricted Auction:</strong>{' '}
-                {auction.restricted ? 'Yes' : 'No'}
-              </li>
-              <li>
-                <strong>Address:</strong> {auction.address}
-              </li>
-              <li>
-                <strong>Start Date:</strong>
-              </li>
-              <li>
-                <strong>Auction Length:</strong>
-                days
-              </li>
-            </ul>
-            <div className="card-actions">
-              <button className="btn btn-primary">Place Bid</button>
+    <div
+      className="hero min-h-screen"
+      style={{ backgroundImage: 'url(/images/hood.jpg' }}
+    >
+      <div className="grid grid-cols-3 gap-4 px-10 mb-10">
+        {auctions?.map((auction) => (
+          <div
+            key={auction.name}
+            className="card w-96 bg-[rgba(80,163,204,0.75)] shadow-xl"
+          >
+            <figure className="px-10 pt-10">
+              <img src="https://5.imimg.com/data5/WS/LU/MY-39504911/mens-woolen-sweater.jpg" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{auction.name}</h2>
+              <p>{auction.description}</p>
+              <h3 className="card-title">
+                Compensation: {auction.compensation}
+              </h3>
+              <ul>
+                <li>
+                  <strong>Restricted Auction:</strong>{' '}
+                  {auction.restricted ? 'Yes' : 'No'}
+                </li>
+                <li>
+                  <strong>Address:</strong> {auction.address}
+                </li>
+                <li>
+                  <strong>Start Date:</strong>{' '}
+                  {auction?.start_date
+                    ? new Date(auction.start_date).toLocaleDateString()
+                    : 'N/A'}
+                </li>
+                <li>
+                  <strong>End Date:</strong>
+                  {auction?.start_date
+                    ? new Date(auction.start_date).toLocaleDateString()
+                    : 'N/A'}
+                </li>
+              </ul>
+              <div className="card-actions">
+                <button className="btn btn-primary">Place Bid</button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
--- pages/job/[id].tsx
+++ pages/job/[id].tsx
@@ -15,32 +15,44 @@ export default function Job() {
 
   return (
     <>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div>
-            <h1 className="text-5xl font-bold">{data?.name}</h1>
-            <p className="py-6">{data?.description}</p>
-            <h3 className="card-title">Compensation: {data?.compensation}</h3>
-            <ul>
-              <li>
-                <strong>Restricted Auction:</strong>{' '}
-                {data?.restricted ? 'Yes' : 'No'}
-              </li>
-              <li>
-                <strong>Address:</strong> {data?.address}
-              </li>
-              <li>
-                <strong>Start Date:</strong> {data.start_date}
-              </li>
-              <li>
-                <strong>Auction Length:</strong> {data.end_date}
-              </li>
-            </ul>
-            <button className="btn btn-primary my-5">Bid Now</button>
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: 'url(/images/hood.jpg' }}
+      >
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="hero bg-[rgba(80,163,204,0.75)] rounded-lg shadow-xl backdrop-blur-[4.3px] border border-[rgba(80,163,204,0.54)] p-6 w-full max-w-2xl">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+              <img
+                src="https://5.imimg.com/data5/WS/LU/MY-39504911/mens-woolen-sweater.jpg"
+                className="max-w-sm rounded-lg shadow-2xl"
+              />
+              <div>
+                <h1 className="text-5xl font-bold">{data?.name}</h1>
+                <p className="py-6">{data?.description}</p>
+                <h3 className="card-title">
+                  Compensation: {data?.compensation}
+                </h3>
+                <ul>
+                  <li>
+                    <strong>Restricted Auction:</strong>{' '}
+                    {data?.restricted ? 'Yes' : 'No'}
+                  </li>
+                  <li>
+                    <strong>Start Date:</strong>{' '}
+                    {data?.start_date
+                      ? new Date(data.start_date).toLocaleDateString()
+                      : 'N/A'}
+                  </li>
+                  <li>
+                    <strong>End Date:</strong>{' '}
+                    {data?.end_date
+                      ? new Date(data.end_date).toLocaleDateString()
+                      : 'N/A'}
+                  </li>
+                </ul>
+                <button className="btn btn-primary my-5">Bid Now</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
