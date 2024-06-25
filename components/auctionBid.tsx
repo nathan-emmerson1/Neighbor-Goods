ac78de50aea43fe2ec8261f9bcafb659691b73c2
Jack Wilson <jxwilx@gmail.com>
2024-06-25 15:19:14 +1200

    functionality done

--- components/auctionBid.tsx
+++ components/auctionBid.tsx
@@ -1,11 +1,59 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
 import { useAuctions } from '@/hooks/fetchAuctions'
-import { JobData } from '@/models/job'
-import * as Popover from '@radix-ui/react-popover'
+import { useState } from 'react'
+import { Job } from '@/models/job'
+import { useRouter } from 'next/router'
+import { useAddBid } from '@/hooks/createbid'
 
-export default function AuctionBid() {
+export default function AuctionBid(job: Job) {
+  const router = useRouter()
+  const [bid, setBid] = useState({
+    id: 0,
+    message: '',
+    compensation: '',
+    user_id: 0,
+    job_id: job.id,
+    date_posted: new Date(),
+  })
+
+  const [message, setMessage] = useState('')
+  const [comp, setComp] = useState('')
+  const { mutate: postBid } = useAddBid()
+
+  const handleSubmit = async () => {
+    setBid({
+      id: 0,
+      message: message,
+      compensation: comp,
+      user_id: 0,
+      job_id: job.id,
+      date_posted: new Date(),
+    })
+    postBid(bid)
+    router.push('/auctions')
+  }
+
   return (
-    
+    <>
+      <input
+        type="text"
+        className=""
+        placeholder="Add a message here!"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        required
+      />
+
+      <input
+        type="text"
+        className=""
+        placeholder="Make an offer!"
+        value={comp}
+        onChange={(e) => setComp(e.target.value)}
+        required
+      />
+      <button onClick={handleSubmit}></button>
+    </>
   )
 }
