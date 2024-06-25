e516815d2e58f504b97a05b55eb5058baf1a6946
Jacob Wingate <jacob.wingate33@gmail.com>
2024-06-25 14:36:33 +1200

    Co-authored-by: joe-smellie <joe-smellie@users.noreply.github.com> Co-authored-by: Jackcwx <Jackcwx@users.noreply.github.com>

--- components/auctionBid.tsx
+++ components/auctionBid.tsx
@@ -2,7 +2,6 @@
 'use client'
 import { useAuctions } from '@/hooks/fetchAuctions'
 import { JobData } from '@/models/job'
-import * as Popover from '@radix-ui/react-popover'
 
 export default function AuctionBid() {
   return (
